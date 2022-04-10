a95ab371bbf0f0b531d95211f1474abed46e984d
schamley <schamley@bu.edu>
2022-04-10 21:04:53 +0200

    chore: add aws creds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scraper
+
+A scraper to grab an html page from a website and store it in s3
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+access_key_id = os.getenv("access_key_id")
+secret_access_key_id = os.getenv("secret_access_key_id")
+
+
+def __main__():
+    pass
