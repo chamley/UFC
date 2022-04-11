b9b8afed583433dae7eeae9ade128d0e76564643
schamley <schamley@bu.edu>
2022-04-11 18:04:20 +0200

    chore:update scraping packages

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+from urllib import response
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import requests
 
 load_dotenv()
 access_key_id = os.getenv("access_key_id")
@@ -7,4 +10,14 @@ secret_access_key_id = os.getenv("secret_access_key_id")
 
 
 def __main__():
-    pass
+    print("starting script")
+    endpoint = "http://ufcstats.com/statistics/events/completed?page=all"
+
+    response = requests.get(endpoint)
+
+    parser = BeautifulSoup(response.content, "html.parser")
+    for element in parser.find_all("a"):
+        print(element.get("href"))
+
+
+__main__()
