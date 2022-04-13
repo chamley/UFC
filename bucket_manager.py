1e9a0781d5089175c846ceb48ee8942105214fd5
schamley <schamley@bu.edu>
2022-04-13 17:13:38 +0200

    fix: fixed bucket mess

--- bucket_manager.py
+++ bucket_manager.py
@@ -1,6 +1,18 @@
 import os
 from dotenv import load_dotenv
 import boto3
+from pprint import pprint
+
+# get all buckets in our s3
+# x = s3r.buckets.all()
+
+# getting objects in one fell swoop is not a thing, we can
+# however get a list of ObjectSummary types that have the description
+# of each object, which can then with the Bucket object be deleted
+# x = sebs_bucket.objects.all()
+
+# Each ObjectSummary o will contain two attributes we need:
+# o.key and o.bucket_name
 
 load_dotenv()
 access_key_id = os.getenv("access_key_id")
@@ -14,19 +26,24 @@ def __main__():
 
 # we grab the latest raw data. We transform in another stage
 def bucket_manager():
-    s3 = boto3.client(
+    s3client = boto3.client(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key_id,
+    )
+    s3resource = boto3.resource(
         "s3",
         region_name="us-east-1",
         aws_access_key_id=access_key_id,
         aws_secret_access_key=secret_access_key_id,
     )
 
-    my_buckets = s3.list_buckets()["Buckets"]
-    for e in my_buckets[:4]:
-        object_dic = s3.list_objects(Bucket=e["Name"])
-        for key, val in object_dic.items():
-
-            print("key: ", key, "   val: ", val)
+    for bucket in s3resource.buckets.all():
+        for o in bucket.objects.all():
+            print(o)
+            response = s3resource.Object(bucket.name, o.key).delete()
+            print(response)
 
 
 __main__()
