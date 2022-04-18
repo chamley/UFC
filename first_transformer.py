d687fa32eedf2ca27c242f29882ab5544bdefb19
schamley <schamley@bu.edu>
2022-04-18 12:02:09 +0200

    feat:add creds

--- first_transformer.py
+++ first_transformer.py
@@ -3,10 +3,21 @@
 # pushes this new file back to S3
 from bs4 import BeautifulSoup
 import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ACCESS_KEY_ID = os.getenv("access_key_id")
+SECRET_ACCESS_KEY_ID = os.getenv("secret_access_key_id")
 
 
 def __main__():
-    pass
+    s3 = boto3.client(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id=ACCESS_KEY_ID,
+        aws_secret_access_key=SECRET_ACCESS_KEY_ID,
+    )
 
 
 # grabs a fight from s3
