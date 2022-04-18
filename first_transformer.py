8763aa0262f7f401c34a8116b742d546b786a990
schamley <schamley@bu.edu>
2022-04-18 11:53:15 +0200

    feat: initial setup of code structure

---
+++ first_transformer.py
@@ -0,0 +1,27 @@
+# The first transformer parses a page and returns a new file for s3 where the data for a fight is
+# parsed and formatted clearly
+# pushes this new file back to S3
+from bs4 import BeautifulSoup
+import boto3
+
+
+def __main__():
+    pass
+
+
+# grabs a fight from s3
+def fetch_fight():
+    pass
+
+
+# turns a fight page into a json object
+def parse_fight():
+    pass
+
+
+# pushes json object back to s3
+def push_fight():
+    pass
+
+
+__main__()
