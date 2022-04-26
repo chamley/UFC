67fd11e0a00aaf018afd26aa8bc84b09a861a3ae
schamley <schamley@bu.edu>
2022-04-26 23:38:25 +0200

    chore:setup db credentials

--- dbhelper.py
+++ dbhelper.py
@@ -1,11 +1,25 @@
 from sqlite3 import connect
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("db_host")
+DB_USERNAME = os.getenv("db_username")
+DB_PASSWORD = os.getenv("db_password")
 
 
 class DBHelper:
     def __init__(self):
-        # self.conn = psycopg2.connect("dbname=test user=postgres")
-        # self.cur = self.conn.cursor()
+        self.conn = psycopg2.connect(
+            dbname="dev",
+            user=DB_USERNAME,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port="5439",
+        )
+        self.cur = self.conn.cursor()
         pass
 
     def query1(self, params):
