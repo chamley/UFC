5ca747cc0bffab22644c7b26173fbe3eb2a3697f
schamley <schamley@bu.edu>
2022-05-02 00:34:29 +0200

    feat:implemented create_Date_dim

--- dbcreator.py
+++ dbcreator.py
@@ -4,22 +4,31 @@ from dbhelper import DBHelper
 def main():
     """The purpose of this script is to fully fledge out version 1.0 of our database"""
     db = DBHelper()
+    create_Date_dim(db)
 
 
-def create_Round_fact():
+def create_Round_fact() -> None:
     pass
 
 
-def create_Fight_dim():
+def create_Fight_dim() -> None:
     pass
 
 
-def create_Fighter_dim():
+def create_Fighter_dim() -> None:
     pass
 
 
-def create_Date_dim():
-    pass
+def create_Date_dim(db: DBHelper) -> None:
+    query = """CREATE TABLE Date_dim (
+                    date_key int primary key,
+                    year varchar(2),
+                    month varchar(2),
+                    day varchar(2),
+                    timestamp datetime
+            """
+    db.getCursor.execute(query)
+    db.getConn.commit()
 
 
 main()
