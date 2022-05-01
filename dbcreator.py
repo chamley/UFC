5919a58237385c29b7915b56678e37cdaef50a52
schamley <schamley@bu.edu>
2022-05-02 00:10:28 +0200

    feat:sketch out dbcreator methods

--- dbcreator.py
+++ dbcreator.py
@@ -1,11 +1,25 @@
-import psycopg2
-
-
 from dbhelper import DBHelper
 
 
 def main():
+    """The purpose of this script is to fully fledge out version 1.0 of our database"""
     db = DBHelper()
+
+
+def create_Round_fact():
+    pass
+
+
+def create_Fight_dim():
+    pass
+
+
+def create_Fighter_dim():
+    pass
+
+
+def create_Date_dim():
+    pass
 
 
 main()
