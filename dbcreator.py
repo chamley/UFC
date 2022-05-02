9646886977eb54c6f5867b7f6595cb6a405b5ee8
schamley <schamley@bu.edu>
2022-05-02 14:39:17 +0200

    fix:removing datetime build query

--- dbcreator.py
+++ dbcreator.py
@@ -14,7 +14,7 @@ def create_Round_fact() -> None:
     pass
 
 
-def create_Date_fim() -> None:
+def populate_Date_dim() -> None:
     pass
 
 
@@ -74,6 +74,43 @@ def create_Date_dim(db: DBHelper) -> None:
             """
     db.getCursor().execute(query)
     db.getConn().commit()
+
+    query = """
+CREATE TABLE DimDate
+	(	DateKey INT primary key,
+		Date DATETIME,
+		FullDateUK CHAR(10), -- Date in dd-MM-yyyy format
+		FullDateUSA CHAR(10),-- Date in MM-dd-yyyy format
+		DayOfMonth VARCHAR(2), -- Field will hold day number of Month
+		DaySuffix VARCHAR(4), -- Apply suffix as 1st, 2nd ,3rd etc
+		DayName VARCHAR(9), -- Contains name of the day, Sunday, Monday
+		DayOfWeekUSA CHAR(1),-- First Day Sunday=1 and Saturday=7
+		DayOfWeekUK CHAR(1),-- First Day Monday=1 and Sunday=7
+		DayOfWeekInMonth VARCHAR(2), --1st Monday or 2nd Monday in Month
+		DayOfWeekInYear VARCHAR(2),
+		DayOfQuarter VARCHAR(3),
+		DayOfYear VARCHAR(3),
+		WeekOfMonth VARCHAR(1),-- Week Number of Month
+		WeekOfQuarter VARCHAR(2), --Week Number of the Quarter
+		WeekOfYear VARCHAR(2),--Week Number of the Year
+		Month VARCHAR(2), --Number of the Month 1 to 12
+		MonthName VARCHAR(9),--January, February etc
+		MonthOfQuarter VARCHAR(2),-- Month Number belongs to Quarter
+		Quarter CHAR(1),
+		QuarterName VARCHAR(9),--First,Second..
+		Year CHAR(4),-- Year value of Date stored in Row
+		YearName CHAR(7), --CY 2012,CY 2013
+		MonthYear CHAR(10), --Jan-2013,Feb-2013
+		MMYYYY CHAR(6),
+		FirstDayOfMonth DATE,
+		LastDayOfMonth DATE,
+		FirstDayOfQuarter DATE,
+		LastDayOfQuarter DATE,
+		FirstDayOfYear DATE,
+		LastDayOfYear DATE,
+		HolidayUSA VARCHAR(50),--Name of Holiday in US
+		HolidayUK VARCHAR(50) Null --Name of Holiday in UK
+	)"""
 
 
 main()
