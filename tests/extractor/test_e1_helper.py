2474d0507bc2d71476552816c6c27bc56784df91
seb <sebchamley@gmail.com>
2022-07-30 19:44:56 +0200

    config: adding config files to test

--- tests/extractor/test_e1_helper.py
+++ tests/extractor/test_e1_helper.py
@@ -1,9 +1,31 @@
 import pytest
+import sys
+from moto import mock_s3
+import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+sys.path.append(".")
 
 
-class get_dates(object):
-    def test_no_empty_elements(self):
+from src.extractor.e1_helper import get_dates
+
+REGION_NAME = os.getenv("REGION_NAME")
+UFC_META_FILES_LOCATION = os.getenv("UFC_META_FILES_LOCATION")
+
+
+@mock_s3
+class TestGetDates(object):
+    def test_normal_usage(self):
+        s3c = boto3.client("s3", REGION_NAME)
+        s3c.create_bucket(Bucket=UFC_META_FILES_LOCATION)
+        s3c.put_object(Bucket=UFC_META_FILES_LOCATION, Key="")
+
+        get_dates()
+
         assert True
 
-    def test_returns_array_of_valid_dates(self):
+    def test_free_tests_weee(object):
+        assert True
         assert True
