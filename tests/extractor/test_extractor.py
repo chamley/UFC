24a7e58e2528937482f16270d75e9584835ff5f4
seb <sebchamley@gmail.com>
2022-07-31 14:08:42 +0200

    chore: init commit create fight page

--- tests/extractor/test_extractor.py
+++ tests/extractor/test_extractor.py
@@ -2,4 +2,21 @@ import sys
 
 sys.path.append(".")
 
+from src.extractor.extractor import (
+    main,
+    stage_layer_1,
+    get_card_urls_dic,
+    get_fight_url_list,
+    create_fight_page,
+    push_fight_page,
+)
 import pytest
+from moto import mock_s3
+
+
+class TestCreateFightPage(object):
+    def test_parses_page_properly(self):
+        assert True
+
+    def test_raises_error_if_bad_url(self):
+        pass
