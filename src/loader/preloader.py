761d1ae04c3e9b4076d7173ff6784fb8016ab920
seb <sebchamley@gmail.com>
2022-09-17 17:07:17 +0200

    refactor: remove scripting capabilities

--- src/loader/preloader.py
+++ src/loader/preloader.py
@@ -22,28 +22,6 @@ STATE = {
 }
 
 
-def my_argument_parser():
-    return parser
-
-
-parser = argparse.ArgumentParser(
-    description="this script is for a lambda that serves as a precursor to a load of fresh data. we STATEther all the metadata and spin up all the temp tables for the load"
-)
-
-arg_group = parser.add_mutually_exclusive_group()
-arg_group.add_argument(
-    "-dates",
-    nargs=2,
-    help="this is the main way of using this program. specify an interval of dates to use when scanning SL2 for files to upload",
-)
-
-arg_group.add_argument(
-    "-dev",
-    action="store_true",
-    help="set DEV_MODE flag to help writing/debugging the program",
-)
-args = my_argument_parser().parse_args()
-
 # run as lambda
 def main(event={}, context=None):
     global STATE  # should only be required here and nowhere else
