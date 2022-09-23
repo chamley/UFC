7fd1d370457b28ed9bfb34c594913baf14d60036
seb <sebchamley@gmail.com>
2022-09-23 16:34:35 +0200

    feat: add parquet func to fight query

--- src/loader/preloader.py
+++ src/loader/preloader.py
@@ -81,6 +81,7 @@ def callCopy(fight_manifest_file_name, round_manifest_file_name):
                 -- iam_role '{REDSHIFT_S3_READ_IAM_ROLE}';
                 access_key_id '{ACCESS_KEY_ID}'
                 secret_access_key '{SECRET_ACCESS_KEY_ID}'
+                FORMAT AS PARQUET
 
             """
     conn = db.getConn()
