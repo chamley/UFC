8ee13f30159337eb2b8c5ae7751ed87a994935e0
seb <sebchamley@gmail.com>
2022-11-11 13:26:36 +0100

    testing: get_keys tested

--- src/t1/ufcstats_t1.py
+++ src/t1/ufcstats_t1.py
@@ -58,12 +58,35 @@ def main(event={}, context=None) -> None:
     STATE = prepstate(event, STATE)
 
     # find all valid keys to transform
-    keys = get_keys()
+    keys = get_keys(STATE)
+
+    # we find idempotency on write as we may be missing a
+    # fight file or a rounds file but not its partner
+
+    print(keys)
 
 
-def get_keys():
+def get_keys(STATE) -> list:
 
-    pass
+    # get keys in SL1 that fall in specified date range
+    valid_sl1_keys = []
+    start = STATE["START_DATE"].year
+    end = STATE["END_DATE"].year
+    years = []  # [2009,2010,2011 ...]
+
+    while start <= end:
+        years.append(start)
+        start += 1
+
+    for y in years:
+        keys = S3C.list_objects_v2(
+            Bucket=STATE["STAGE_LAYER_ONE"], Prefix=f"fight-{y}"
+        ).get("Contents")
+        if keys:
+            for k in keys:
+                valid_sl1_keys.append(k["Key"])
+    print(valid_sl1_keys)
+    return valid_sl1_keys
 
 
 def prepstate(event, STATE) -> dict:
@@ -78,7 +101,7 @@ def prepstate(event, STATE) -> dict:
         ):
             raise ValueError
     except TypeError:
-        raise ValueError("Invalid Dates")
+        raise ValueError("invalid dates")
 
     STATE["START_DATE"] = date.fromisoformat(event["dates"]["start"])
     STATE["END_DATE"] = date.fromisoformat(event["dates"]["end"])
--- tests/t1/test_first_transformer.py
+++ tests/t1/test_first_transformer.py
@@ -1,3 +1,7 @@
+# ToDo:
+# 1. We should probably test somewhere for credentials in a meaningful way as it is a fairly probably source of error
+
+
 import sys
 
 sys.path.append(".")
@@ -10,10 +14,12 @@ from datetime import date
 
 import pytest
 from configfile import STAGE_LAYER_ONE, STAGE_LAYER_TWO, REGION_NAME
-from src.t1.ufcstats_t1 import prepstate
-
+from src.t1.ufcstats_t1 import prepstate, get_keys
+from src.t1.t1_exceptions import InvalidDates
 
 # Default state (what we start off with at the top of the script/load from config)
+
+
 def return_default_state():
     return {
         "STAGE_LAYER_ONE": STAGE_LAYER_ONE,
@@ -31,7 +37,7 @@ class TestPrepstate(object):
         "STATE, event",
         [
             (return_default_state(), {}),
-            (return_default_state(), {"asdf": "ass"}),
+            (return_default_state(), {"asdf": "asaas"}),
             (return_default_state(), {"dates": "hello"}),
             (return_default_state(), {"dates": {"start": "2021-09-08", "end": "asdf"}}),
             (return_default_state(), {"dates": {"end": "2021-09-08", "start": "asdf"}}),
@@ -72,4 +78,50 @@ class TestPrepstate(object):
     def test_sets_dates_approriately(self, STATE, event, expected):
         event = defaultdict(lambda: None, event)
         actual = prepstate(event, STATE)
+        assert actual == expected
+
+
+from mock_data.mock_data_get_keys import mock_keys
+
+
+class TestGet_Keys(object):
+    @pytest.mark.parametrize(
+        "STATE, expected",
+        [
+            (
+                {
+                    **return_default_state(),
+                    "START_DATE": date(1990, 1, 1),
+                    "END_DATE": date(2001, 1, 1),
+                },
+                mock_keys["keys_1"],
+            ),
+            (
+                {
+                    **return_default_state(),
+                    "START_DATE": date(1985, 1, 1),
+                    "END_DATE": date(1985, 1, 1),
+                },
+                [],
+            ),
+            (
+                {
+                    **return_default_state(),
+                    "START_DATE": date(2050, 1, 1),
+                    "END_DATE": date(2050, 1, 1),
+                },
+                [],
+            ),
+            (
+                {
+                    **return_default_state(),
+                    "START_DATE": date(2019, 1, 1),
+                    "END_DATE": date(2022, 1, 1),
+                },
+                mock_keys["keys_2"],
+            ),
+        ],
+    )
+    def test_proper_keys_retrieved(self, STATE, expected):
+        actual = get_keys(STATE)
         assert actual == expected
