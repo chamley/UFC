86f32b7eadc0132602d0c1dd530b9f6930fb91f0
seb <sebchamley@gmail.com>
2022-11-14 18:51:27 +0100

    modularized config for load tables

---
+++ src/loader/configfile.py
@@ -0,0 +1,11 @@
+REGION_NAME = "us-east-1"
+STAGE_LAYER_ONE = "ufc-big-data"
+STAGE_LAYER_TWO = "ufc-big-data-2"
+UFC_META_FILES_LOCATION = "ufc-meta-files"
+LOAD_MANIFEST_FOLDER = "load_manifests"
+E1_CSV_OPT_DATE_FOLDER_PATH = "e1-dates"
+REDSHIFT_S3_READ_IAM_ROLE = (
+    "arn:aws:iam::aws:policy/AmazonRedshiftAllCommandsFullAccess"
+)
+UFCSTATS_ROUND_SOURCE_TABLE_NAME = "round_source"
+UFCSTATS_FIGHT_SOURCE_TABLE_NAME = "fight_source"
