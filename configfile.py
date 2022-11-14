3e98223b333cbef2c49fdce607877425d2966a3b
seb <sebchamley@gmail.com>
2022-11-14 19:52:17 +0100

    added docs to configfile

--- configfile.py
+++ configfile.py
@@ -1,11 +1,13 @@
-# general architecture names
 config_settings = {
+    # general settings
     "REGION_NAME": "us-east-1",
+    # S3 bucket names
     "STAGE_LAYER_ONE": "ufc-big-data",
     "STAGE_LAYER_TWO": "ufc-big-data-2",
     "UFC_META_FILES_LOCATION": "ufc-meta-files",
     "LOAD_MANIFEST_FOLDER": "load_manifests",
     "E1_CSV_OPT_DATE_FOLDER_PATH": "e1-dates",
+    # roles
     "REDSHIFT_S3_READ_IAM_ROLE": (
         "arn:aws:iam::aws:policy/AmazonRedshiftAllCommandsFullAccess"
     ),
