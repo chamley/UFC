d5cfad587b5a93e9c88ba11894ac048bc22e43da
sebchamley@gmail.com <sebchamley@gmail.com>
2022-11-22 17:00:13 +0000

    ay mamasita

--- db/dbt/models/example/my_first_dbt_model.sql
+++ db/dbt/models/example/my_first_dbt_model.sql
@@ -6,22 +6,5 @@
 
     Try changing "table" to "view" below
 */
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+{{ config(materialized="table") }}
+ select * from public.round_source
